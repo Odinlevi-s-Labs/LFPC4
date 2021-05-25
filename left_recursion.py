7e0fb831d82c5458d7e19e63a6c1bb5bf8af51ef
Odinlevi <odinlevi@gmail.com>
2021-05-25 22:36:41 +0300

    done

---
+++ left_recursion.py
@@ -0,0 +1,39 @@
+from left_factoring import *
+
+
+def left_recursion(nonterminal_list,production_list):
+    new_production_list = []
+    new_nonterminal_list = []
+    nonterminal_with_LR_list = [left for production in production_list for
+        left,right in production.items() if(right.startswith(left))]
+
+    for nonterminal in nonterminal_with_LR_list:
+        counter = 1
+        while(1): #this loop is meant to create a proper new_nonterminal that did not exist
+            new_nonterminal = nonterminal+str(counter)
+            if new_nonterminal in nonterminal_list:
+                counter+=1
+            else:
+                new_nonterminal_list.append(new_nonterminal)
+                break
+        for production in production_list:
+            for left,right in production.items():
+                if(left == nonterminal):
+                    if(right.startswith(left)):
+                        new_production_list.append({new_nonterminal:right[1:]+new_nonterminal})
+                        new_production_list.append({new_nonterminal:""})
+                    else:
+                        new_production_list.append({nonterminal:right+new_nonterminal})
+    # print(nonterminal_with_LR_list)
+    # print(new_production_list)
+    # print(new_nonterminal_list)
+    production_list = [production for production in production_list for
+        left,right in production.items() if left not in nonterminal_with_LR_list] #removed the changed productions
+    production_list += new_production_list #add new productions
+    nonterminal_list += new_nonterminal_list #add new nonterminals
+    return nonterminal_list, production_list
+
+
+input_dict["Grammar"]["Nonterminal"],input_dict["Grammar"]["Productions"] = left_recursion(input_dict["Grammar"]["Nonterminal"],input_dict["Grammar"]["Productions"])
+print("\nData after left recursion\n")
+pprint(input_dict)
---
+++ read_input.py
@@ -0,0 +1,17 @@
+import json
+from pprint import pprint
+
+
+#Returns data dictionary
+def read_json_file(file_name):
+    fo = open(file_name,"r") #Open file json
+    data_string = fo.read() #Obtain string from file
+    data = json.loads(data_string) #from string to data
+    fo.close()
+    return data
+
+
+input_dict = read_json_file("input.json")
+
+print('\nInput Data\n')
+pprint(input_dict)
